de1d71ba9c43873a954853760243d26a87595c19
sushanthumiitech <sushantsrk2001@gmail.com>
2025-05-08 01:10:55 +0530

    Auth UI Screen

---
+++ pages/auth.tsx
@@ -0,0 +1,78 @@
+import Input from "@/components/Input";
+import { useCallback, useState } from "react";
+
+const Auth = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+  return (
+    <div
+      className="relative h-full w-full bg-no-repeat bg-center bg-cover"
+      style={{
+        background: "url(/images/hero.png",
+      }}>
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" className="h-36" />
+        </nav>
+        <div className="flex justify-center -mt-16">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  id="username"
+                  label="Username"
+                  onChange={(event: any) => {
+                    setUsername(event.target.value);
+                  }}
+                  value={username}
+                />
+              )}
+              <Input
+                id="email"
+                label="Email address"
+                type="email"
+                onChange={(event: any) => {
+                  setEmail(event.target.value);
+                }}
+                value={email}
+              />
+              <Input
+                id="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={(event: any) => {
+                  setPassword(event.target.value);
+                }}
+              />
+            </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              First time using Netflix?{" "}
+              <span
+                onClick={toggleVariant}
+                className="text-white hover:underline cursor-pointer">
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold">Netflix Clone</h1>
+      <h1 className="text-3xl font-bold text-red-500">Netflix Clone</h1>
     </>
   );
 }
